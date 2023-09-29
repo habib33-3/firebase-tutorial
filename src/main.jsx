fa6b0bb62d7346f9f1cbab9edfde557fe605337c
Habibur Rahman <habib.swi16@gmail.com>
2023-09-29 21:01:03 +0600

    created navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,31 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./components/Root";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/register",
+        element: <Register />,
+      },
+
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
   },
 ]);
 
